6a733b6688a52daaee8a98a7cb43ff3f4733a41d
fdn.nikita@gmail.com <fdn.nikita@gmail.com>
2023-06-22 00:52:34 +0300

    db config

--- app/src/Controller/MainController.php
+++ app/src/Controller/MainController.php
@@ -2,15 +2,27 @@
 
 namespace App\Controller;
 
+use Doctrine\DBAL\Exception;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
 {
     #[Route('', name: 'index')]
-    public function indexAction()
+    public function indexAction(
+        EntityManagerInterface $em
+    )
     {
-        return new Response("It Works!");
+        try {
+            $em->getConnection()->connect();
+        } catch (Exception $e) {
+            $errors = (string) $e;
+        }
+        return new JsonResponse([
+            'db_errors'        => $em->getConnection()->isConnected() ? 'connected' : $errors ?? 'unknown error',
+        ]);
     }
 }
