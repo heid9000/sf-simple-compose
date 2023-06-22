544cb4328512465f874957d88643b96a3a5a0076
fdn.nikita@gmail.com <fdn.nikita@gmail.com>
2023-06-22 15:40:56 +0300

    fixex

--- Makefile
+++ Makefile
@@ -5,6 +5,6 @@ down:
 rebuild:
 	docker-compose up -d --build --force-recreate --no-deps
 cli:
-	docker-compose exec php-fpm-gd sh
+	docker-compose exec php-fpm-gd su - admin -c "cd app && sh"
 test_db:
 	docker-compose exec php-fpm-gd bin/console dbal:run-sql "select 'succesfully connected!' as result;"
--- app/src/Controller/MainController.php
+++ app/src/Controller/MainController.php
@@ -6,7 +6,6 @@ use Doctrine\DBAL\Exception;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
